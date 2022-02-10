e4fcfec0e45208f7243d88b942c4b4831fa7e6ef
geogschmidtdev <webgsch1@web.de>
2022-02-10 16:51:03 +0100

    Locomotion improvements

---
+++ Assets/Scripts/LocomotionController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+    public XRController leftTeleportRay;
+    public XRController rightTeleportRay;
+    public InputHelpers.Button teleportationActivationButton;
+    
+    private float activationThreshold = 0.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftTeleportRay)
+        {
+            leftTeleportRay.gameObject.SetActive(CheckIfActive(leftTeleportRay));
+        }
+
+        if (rightTeleportRay)
+        {
+            rightTeleportRay.gameObject.SetActive(CheckIfActive(rightTeleportRay));
+        }
+    }
+
+    public bool CheckIfActive(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportationActivationButton, out bool result, activationThreshold);
+        return result;
+    }
+}
