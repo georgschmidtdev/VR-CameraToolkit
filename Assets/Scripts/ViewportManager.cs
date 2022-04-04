e2a3dd4950dedc0c182fa140c622a77a82c1e2ac
geogschmidtdev <webgsch1@web.de>
2022-04-04 19:33:18 +0200

    Initial camera settings interface

--- Assets/Scripts/ViewportManager.cs
+++ Assets/Scripts/ViewportManager.cs
@@ -14,11 +14,16 @@ public class ViewportManager : MonoBehaviour
     public GameObject framerateIndicator;
     public GameObject focalLengthIndicator;
     public GameObject aspectRatioIndicator;
+    public int[] framerates = new int[]{24, 25, 30, 50, 60, 120};
     public enum AspectRatio {Standard, Square, HDTV, Still35, Widescreen, Film35, Panavision};
     public AspectRatio aspectRatio;
-    public float defaultFocalLength = 35f;
-    public float customFocalLength = 35f;
+    public enum FocalLength {Default, Wide, Classic, Normal, Portrait, Tele};
+    public FocalLength focalLength;
+    public GameObject framerateDropdown;
+    public GameObject aspectRatioDropdown;
+    public GameObject focalLengthDropdown;
 
+    private GameObject vrCamera;
     private RectTransform viewport;
     private RectTransform indicators;
     private static Image recordingDot;
@@ -36,31 +41,28 @@ public class ViewportManager : MonoBehaviour
     private Vector2 panavisionRatio = new Vector2(2.39f, 1.0f);
     private Dictionary<AspectRatio, Vector2> aspectRatioDictionary;
     private float currentAspectRatio = 0.0f;
-    private GameObject vrCamera;
+    private float defaultFocal = 36.0f;
+    private float wideFocal = 25.0f;
+    private float classicFocal = 36.0f;
+    private float normalFocal = 50.0f;
+    private float portraitFocal = 80.0f;
+    private float teleFocal = 135.0f;
+    private Dictionary<FocalLength, float> focalLengthDictionary;
 
     // Start is called before the first frame update
     void Start()
     {
-        aspectRatioDictionary = new Dictionary<AspectRatio, Vector2>();
-        aspectRatioDictionary.Add(AspectRatio.Standard, standardRatio);
-        aspectRatioDictionary.Add(AspectRatio.Square, squareRatio);
-        aspectRatioDictionary.Add(AspectRatio.HDTV, hdtvRatio);
-        aspectRatioDictionary.Add(AspectRatio.Still35, still35Ratio);
-        aspectRatioDictionary.Add(AspectRatio.Widescreen, widescreenRatio);
-        aspectRatioDictionary.Add(AspectRatio.Film35, film35Ratio);
-        aspectRatioDictionary.Add(AspectRatio.Panavision, panavisionRatio);
+        VariableSetup();
+        UpdateViewport();
+        UpdateInterface();
 
+        vrCamera = GameObject.FindWithTag("MainCamera");
         viewport = viewportCanvas.GetComponent<RectTransform>();
         indicators = indicatorCanvas.GetComponent<RectTransform>();
-        vrCamera = GameObject.FindWithTag("MainCamera");
-
         recordingDot = recordingIndicator.GetComponent<Image>();
         framerateText = framerateIndicator.GetComponent<TextMeshProUGUI>();
         focalLengthText = focalLengthIndicator.GetComponent<TextMeshProUGUI>();
         aspectRatioText = aspectRatioIndicator.GetComponent<TextMeshProUGUI>();
-
-        UpdateViewport();
-        UpdateInterface();
     }
 
     // Update is called once per frame
@@ -73,7 +75,27 @@ public class ViewportManager : MonoBehaviour
         }
     }
 
-    private void UpdateViewport()
+    void VariableSetup()
+    {
+        aspectRatioDictionary = new Dictionary<AspectRatio, Vector2>();
+        aspectRatioDictionary.Add(AspectRatio.Standard, standardRatio);
+        aspectRatioDictionary.Add(AspectRatio.Square, squareRatio);
+        aspectRatioDictionary.Add(AspectRatio.HDTV, hdtvRatio);
+        aspectRatioDictionary.Add(AspectRatio.Still35, still35Ratio);
+        aspectRatioDictionary.Add(AspectRatio.Widescreen, widescreenRatio);
+        aspectRatioDictionary.Add(AspectRatio.Film35, film35Ratio);
+        aspectRatioDictionary.Add(AspectRatio.Panavision, panavisionRatio);
+
+        focalLengthDictionary = new Dictionary<FocalLength, float>();
+        focalLengthDictionary.Add(FocalLength.Default, defaultFocal);
+        focalLengthDictionary.Add(FocalLength.Wide, wideFocal);
+        focalLengthDictionary.Add(FocalLength.Classic, classicFocal);
+        focalLengthDictionary.Add(FocalLength.Normal, normalFocal);
+        focalLengthDictionary.Add(FocalLength.Portrait, portraitFocal);
+        focalLengthDictionary.Add(FocalLength.Tele, teleFocal);
+    }
+
+    void UpdateViewport()
     // Apply changes
     {
         currentAspectRatio = aspectRatioDictionary[aspectRatio].x / aspectRatioDictionary[aspectRatio].y;
@@ -89,7 +111,7 @@ public class ViewportManager : MonoBehaviour
         viewportCamera.aspect = currentAspectRatio;
     }
 
-    private void UpdateInterface()
+    void UpdateInterface()
     {
         List<float> cameraSettings = new List<float>();
         cameraSettings = AnimationRecorder.GetCameraSettings();
