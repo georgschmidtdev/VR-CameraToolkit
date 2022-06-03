87069ee80ef75233cdbdafe48edf679638ede227
geogschmidtdev <webgsch1@web.de>
2022-06-03 13:18:21 +0200

    Editor tools update
    Recording via editor tools possible

--- Assets/Editor/CustomEditors/VRCTShortcuts.cs
+++ Assets/Editor/CustomEditors/VRCTShortcuts.cs
@@ -5,28 +5,19 @@ using UnityEditor;
 
 public class VRCTShortcuts : EditorWindow
 {
-    int mode = 0;
-    string[] modeOptions = new string[] {"Default", "Recording", "Visualization"};
     float frameRate = 24.0f;
     Vector2 sensorSize = new Vector2(5.0f, 5.0f);
     Vector2 aspectRatio = new Vector2(1.0f, 1.0f);
     float focalLength = 25.0f;
-    int animation = 0;
-    string[] animationOptions;
     
     private bool recording = false;
+    private GameObject index;
+    private GameObjectIndex gameObjectIndex;
     private GameObject rig;
     private GameObject virtualViewport;
     private Camera viewportCamera;
     private AspectRatioManager aspectRatioManager;
-    private ModeManager modeManager;
-    private ViewportManager viewportManager;
     private AnimationRecorder animationRecorder;
-    private AnimationManager animationManager;
-    private List<GameObject> animations;
-    private List<GameObject> previews;
-    private GameObject animationContainer;
-    private GameObject previewContainer;
 
     [MenuItem("Tools/VRCTShortcuts")]
 
@@ -37,18 +28,8 @@ public class VRCTShortcuts : EditorWindow
 
     private void OnGUI()
     {
-        InitialSetup();
-
-        EditorGUI.BeginChangeCheck();
-        mode = EditorGUILayout.Popup("Mode", mode, modeOptions);
-        if (EditorGUI.EndChangeCheck())
-        {
-            UpdateMode();
-        }
-
-        EditorGUILayout.Space();
-    
-        using (new EditorGUI.DisabledScope(mode != 1))
+        InitialSetup();    
+        using (new EditorGUI.DisabledScope(!CheckPlayMode()))
         {
             EditorGUI.BeginChangeCheck();
             frameRate = EditorGUILayout.FloatField("Framerate", frameRate);
@@ -60,6 +41,8 @@ public class VRCTShortcuts : EditorWindow
             {
                 UpdateCameraSettings();
             }
+
+            EditorGUILayout.Space(20);
 
             if (!recording)
             {
@@ -76,57 +59,17 @@ public class VRCTShortcuts : EditorWindow
                 }
             }
         }
-
-        using (new EditorGUI.DisabledScope(mode != 2))
-        {
-            EditorGUI.BeginChangeCheck();
-            if (animations.Count != 0)
-            {
-                animation = EditorGUILayout.Popup("Animation", animation, animationOptions);
-                if (EditorGUI.EndChangeCheck())
-                {
-                    UpdateAnimationPreview();
-                }
-            }
-        }
     }
 
     private void InitialSetup()
     {
-        rig = GameObject.FindGameObjectWithTag("Rig");
-        virtualViewport = GameObject.FindGameObjectWithTag("VirtualViewport");
+        index = GameObject.FindGameObjectWithTag("Index");
+        gameObjectIndex = index.GetComponent<GameObjectIndex>();
+        rig = gameObjectIndex.rig;
+        virtualViewport = gameObjectIndex.virtualViewport;
         viewportCamera = virtualViewport.GetComponent<Camera>();
         aspectRatioManager = virtualViewport.GetComponent<AspectRatioManager>();
-        modeManager = rig.GetComponent<ModeManager>();
-        viewportManager = rig.GetComponent<ViewportManager>();
         animationRecorder = rig.GetComponent<AnimationRecorder>();
-        animationManager = rig.GetComponent<AnimationManager>();
-        animationContainer = GameObject.FindGameObjectWithTag("LineContainer");
-        previewContainer = GameObject.FindGameObjectWithTag("PreviewContainer");
-
-
-        UpdateMode();
-    }
-
-    private void UpdateMode()
-    {
-        if (recording)
-        {
-            StopRecording();
-        }
-        if (mode == 0)
-        {
-            modeManager.interactionMode = ModeManager.InteractionMode.explore;
-        }
-        else if (mode == 1)
-        {
-            modeManager.interactionMode = ModeManager.InteractionMode.recording;
-        }
-        else if (mode == 2)
-        {
-            modeManager.interactionMode = ModeManager.InteractionMode.visualizing;
-            Visualize();
-        }
     }
 
     private void StartRecording()
@@ -144,34 +87,6 @@ public class VRCTShortcuts : EditorWindow
         Debug.Log("Stopped Recording");
     }
 
-    private void Visualize()
-    {
-        if (CheckPlayMode())
-        {
-            animationManager.BuildAnimations();
-            animations = new List<GameObject>();
-            previews = new List<GameObject>();
-    
-            foreach (Transform item in animationContainer.transform)
-            {
-                animations.Add(item.gameObject);
-            }
-    
-            foreach (Transform item in previewContainer.transform)
-            {
-                previews.Add(item.gameObject);
-            }
-    
-            animationOptions = new string[animations.Count];
-            for (int index = 0; index < animations.Count; index++)
-            {
-                animationOptions[index] = animations[index].name;
-            }
-    
-            Debug.Log("Visualizing");
-        }
-    }
-
     private void UpdateCameraSettings()
     {
         Debug.Log("Updated Camera");
@@ -179,15 +94,6 @@ public class VRCTShortcuts : EditorWindow
         viewportCamera.sensorSize = sensorSize;
         aspectRatioManager.aspectRatio = aspectRatio.x / aspectRatio.y;
         viewportCamera.focalLength = focalLength;
-    }
-
-    private void UpdateAnimationPreview()
-    {
-        if (CheckPlayMode())
-        {
-            animationManager.ToggleVisibility(animations[animation].name);
-            animationManager.UpdatePreviewAnimation(animations[animation].name);
-        }
     }
 
     private bool CheckPlayMode()
---
+++ Assets/Scripts/GameObjectIndex.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectIndex : MonoBehaviour
+{
+    public GameObject rig;
+    public GameObject virtualViewport;
+    public GameObject lineContainer;
+    public GameObject extractorContainer;
+    public GameObject previewContainer;
+    public RenderTexture previewCameraRenderTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
